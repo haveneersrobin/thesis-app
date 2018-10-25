2c94635206b0b3f4e017ad20b8d4a91edb7214cc
Robin Haveneers <haveneersrobin@gmail.com>
2018-10-25 22:00:42 +0200

    Adding first screens and init project

--- App.js
+++ App.js
@@ -1,21 +1,82 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, View } from "react-native";
+import { createStackNavigator } from "react-navigation";
+import styled from "styled-components";
+import ArtistSelectionScreen from "./screens/ArtistSelectionScreen";
+import Button from "./components/Button";
+import { LinearGradient, AppLoading } from "expo";
+import { Entypo } from "@expo/vector-icons";
+import { Font } from "expo";
 
-export default class App extends React.Component {
+const StyledView = styled(View)`
+  flex: 1;
+  width: 100%;
+  height: 100%;
+  align-items: center;
+  justify-content: flex-end;
+  margin-bottom: 140;
+`;
+
+function cacheFonts(fonts) {
+  return fonts.map(font => Font.loadAsync(font));
+}
+
+class HomeScreen extends React.Component {
+  state = {
+    isReady: false
+  };
+
+  async _loadAssetsAsync() {
+    const fontAssets = cacheFonts([
+      { droid: require("./assets/fonts/DroidSans.ttf") },
+      { "droid-bold": require("./assets/fonts/DroidSans-Bold.ttf") }
+    ]);
+    await Promise.all([...fontAssets]);
+  }
+
   render() {
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._loadAssetsAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      );
+    }
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <LinearGradient
+        style={{
+          position: "absolute",
+          left: 0,
+          right: 0,
+          bottom: 0,
+          height: "100%"
+        }}
+        start={[1, 0]}
+        end={[0, 1]}
+        colors={["#8360C3", "rgba(46, 191, 145, 0.4)"]}
+      >
+        <StyledView>
+          <Button bgColor={"#23CF5F"} text={"Log in with Spotify"}>
+            <Entypo name="spotify-with-circle" size={24} color="white" />
+          </Button>
+        </StyledView>
+      </LinearGradient>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+export default createStackNavigator(
+  {
+    Home: { screen: HomeScreen },
+    ArtistSelection: { screen: ArtistSelectionScreen }
   },
-});
+  {
+    initialRouteName: "Home",
+    headerMode: "none",
+    navigationOptions: {
+      headerVisible: false
+    }
+  }
+);
